f9de1734d3565ee0984e9660c7cb691d5de47804
Jay Stanley <jay.s.stanley.3@gmail.com>
2022-02-01 19:53:45 -0500

    dev 0.03d

--- docker-shell-scripts/service_entrypoint.sh
+++ docker-shell-scripts/service_entrypoint.sh
@@ -1,19 +1,23 @@
 #!/usr/bin/env bash
-# exec /docker/install_jupyter_service.sh
-# exec pip install -e /bipca 
+
+GROUPID=1000
+GROUPID=${USERID:-GROUPID}
 set -ef -o pipefail
 source _activate_current_env.sh
-pip install -e /bipca
+echo "Installing the python module mounted at /bipca"
+pip install -e /bipca > pip.log
 if [ "$JUPYTER" = true ] ; then
     echo "Jupyter enabled." 
 else
+    echo "Jupyter disabled."
     touch /etc/services.d/jupyter/down
 fi
 if [ "$RSTUDIO" = true ] ; then
     echo "rstudio enabled." 
 else
+    echo "rstudio disabled."
     touch /etc/services.d/rstudio/down
 fi
 
 
-exec /init sudo -E -u $USER -s $@ 
+exec /init sudo -u $USER -s $@ 
