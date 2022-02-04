e6af06f8c40661e99943c5950614a711fbc1500d
Jay Stanley <jay.s.stanley.3@gmail.com>
2022-02-03 19:42:15 -0500

    rm incrementing, add manual version, bipca ver

--- build.sh
+++ build.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/env bash
 DEFAULTPAT=''
 GITHUB_PAT=${GITHUB_PAT:-$DEFAULTPAT}
-DEFAULTINCREMENT='true'
-INCREMENT=${INCREMENT:-$DEFAULTINCREMENT}
-DEFAULTPREFIX='dev'
-PREFIX=${PREFIX:-$DEFAULTPREFIX}
 DEFAULTSTAGE='final'
-STAGE=${STAGE:-$DEFAULTSTAGE}
-if [ $INCREMENT = 'true' ]; then
-    new_version=$(docker images | awk -v pat=$PREFIX '{if( ($1 == "bipca-experiment") && ( $2 ~ pat )) {  print substr($2, 1, length($2)-1) 1+substr($2, length($2)); exit}}')
-else
-    new_version=$(docker images | awk -v pat=$PREFIX '{if( ($1 == "bipca-experiment") && ( $2 ~ pat )) {  print $2; exit}}')
-fi
-echo "Building version $new_version to stage $STAGE"
-docker build -t bipca-experiment:$new_version --build-arg GITHUB_PAT=$GITHUB_PAT --target=$STAGE -f dockerfiles/bipca-base0.03.dockerfile  . > bipca-experiment:$new_version.log && echo "Build completed"
+STAGE=${STAGE:=final}
+dockerfile_version=${dockerfile_version:=''}
+dockerfile="bipca-base$dockerfile_version.dockerfile"
+dockerfile_path=${dockerfile_path:='dockerfiles/'}
+dockerfile="${dockerfile_path}bipca-base$dockerfile_version.dockerfile"
+BIPCA_VERSION=${BIPCA_VERSION:=}
+echo "Building version $new_version to stage $STAGE from $dockerfile"
+docker build -t bipca-experiment:$new_version --build-arg BIPCA_VERSION=$BIPCA_VERSION --build-arg GITHUB_PAT=$GITHUB_PAT --target=$STAGE -f $dockerfile  . > bipca-experiment:$new_version.log && echo "Build completed"
 
 
