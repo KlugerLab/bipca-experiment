aea74e26a5348cfa6ca139d0f7dc7f9e41dcb921
Jay Stanley <jay.s.stanley.3@gmail.com>
2022-02-03 19:43:02 -0500

    move pip install to cont-init,add logging

--- docker-shell-scripts/service_entrypoint.sh
+++ docker-shell-scripts/service_entrypoint.sh
@@ -4,8 +4,7 @@ GROUPID=1000
 GROUPID=${USERID:-GROUPID}
 set -ef -o pipefail
 source _activate_current_env.sh
-echo "Installing the python module mounted at /bipca"
-pip install -e /bipca > pip.log
+
 if [ "$JUPYTER" = true ] ; then
     echo "Jupyter enabled." 
 else
