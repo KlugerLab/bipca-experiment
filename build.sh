4fa404c22e86cc7d1f26a41a547b01bdea246584
Jay Stanley <jay.s.stanley.3@gmail.com>
2022-02-04 16:13:12 -0500

    add no-cache option to build

--- build.sh
+++ build.sh
@@ -8,7 +8,12 @@ dockerfile="bipca-base$dockerfile_version.dockerfile"
 dockerfile_path=${dockerfile_path:='dockerfiles/'}
 dockerfile="${dockerfile_path}bipca-base$dockerfile_version.dockerfile"
 BIPCA_VERSION=${BIPCA_VERSION:=}
-echo "Building version $new_version to stage $STAGE from $dockerfile"
-docker build -t bipca-experiment:$new_version --build-arg BIPCA_VERSION=$BIPCA_VERSION --build-arg GITHUB_PAT=$GITHUB_PAT --target=$STAGE -f $dockerfile  . > bipca-experiment:$new_version.log && echo "Build completed"
+CACHE=${CACHE:=TRUE}
 
+echo "Building version $new_version to stage $STAGE from $dockerfile"
+if [ "$CACHE" == "TRUE" ]; then
+    docker build -t bipca-experiment:$new_version --build-arg BIPCA_VERSION=$BIPCA_VERSION --build-arg GITHUB_PAT=$GITHUB_PAT --target=$STAGE -f $dockerfile  . > bipca-experiment:$new_version.log && echo "Build completed"
+else
+    docker build --no-cache -t bipca-experiment:$new_version --build-arg BIPCA_VERSION=$BIPCA_VERSION --build-arg GITHUB_PAT=$GITHUB_PAT --target=$STAGE -f $dockerfile  . > bipca-experiment:$new_version.log && echo "Build completed"
+fi
 
